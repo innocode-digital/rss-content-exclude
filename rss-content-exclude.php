4435d43a1b7f8c6c78bbf2e911480d7585818932
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2021-02-23 13:02:48 +0200

    prepare for packagist

--- rss-content-exclude.php
+++ rss-content-exclude.php
@@ -1,23 +1,30 @@
 <?php
-/*
- * Plugin Name: RSS Content Exclude
- * Description: Exclude from rss feed Divi builder sections with class "RSS-NoFollow"
- * Author:      Innocode AS
- * Author URI:  https://innocode.no
- * Version:     1.1
+/**
+ * Plugin Name: Divi RSS Content Exclude
+ * Description: Exclude from RSS feed Divi builder sections with class "RSS-NoFollow".
+ * Version: 1.1.0
+ * Author: Innocode
+ * Author URI: https://innocode.com
+ * Tested up to: 5.6.2
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-add_filter( 'et_module_shortcode_output', function ( $output, $render_slug, $this_shortcode ) {
-	if ( is_feed() ) {
-		/**
-		 * @var $this_shortcode ET_Builder_Element
-		 */
-		$classes = isset( $this_shortcode->classname ) ? $this_shortcode->classname : [];
-		if ( $classes && in_array( 'RSS-NoFollow', $classes ) ) {
-			$output = '';
-		}
+add_filter( 'et_module_shortcode_output', function ( $output, $render_slug, $shortcode ) {
+	if ( ! is_feed() ) {
+		return $output;
 	}
 
+    /**
+     * @var ET_Builder_Element $shortcode
+     */
+    if (
+        ! empty( $shortcode->classname ) &&
+        is_array( $shortcode->classname ) &&
+        in_array( 'RSS-NoFollow', $shortcode->classname )
+    ) {
+        $output = '';
+    }
 
 	return $output;
 }, 10, 3 );
